efeac8b83738e21b7ca9466640770b41ab57785e
Deniz bro bro <140730727+Denizbrobro@users.noreply.github.com>
2024-02-13 22:36:46 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+import discord
+import pyfiglet
+from discord.ext import commands
+import openai
+from config import openapikey, MCTOKEN
+
+# OpenAI API Key
+openai.api_key = openapikey
+
+DBBS_text = pyfiglet.figlet_format(text="Denizbrobro ------------ STUDIOS")
+
+
+
+intents = discord.Intents.all()
+intents.message_content = True
+intents.members = True
+
+# Create discord bot
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    print(DBBS_text)
+    await bot.change_presence(activity=discord.Game(name="Has Brain🧠"))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return  # Mesaj bot tarafından gönderildiyse, işlem yapma
+
+    question = message.content
+    
+    try:
+        # GPT-3.5-turbo-0125 modelini kullanarak bir yanıt oluştur
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question}
+            ]
+        )
+
+        # Yanıtı al
+        answer = response['choices'][0]['message']['content'].strip()
+
+        # Cevabı Discord'a gönder
+        await message.channel.send(answer)
+
+    except openai.error.OpenAIError as e:
+        await message.channel.send(f'Hata: {e.message}')
+
+# Discord botunu çalıştır
+bot.run(MCTOKEN)
