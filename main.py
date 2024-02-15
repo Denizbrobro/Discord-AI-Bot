88ed73d3bcf0bcec43e45026cff8dcf4046f106f
Deniz bro bro <140730727+Denizbrobro@users.noreply.github.com>
2024-02-15 20:06:08 +0300

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pyfiglet
 from discord.ext import commands
 import openai
 from config import *
+from discord.interactions import Interaction
 
 # OpenAI API Key
 openai.api_key = openapikey
@@ -16,16 +17,8 @@ intents.members = True
 # Create discord bot
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    print(DBBS_text)
-    await bot.change_presence(activity=discord.Game(name="Has Brain🧠"))
-
-@bot.command(name='ask')
-async def ask(ctx, *args):
-    question = ' '.join(args)
-
+@bot.tree.command(name="ask", description="If you have a problem, ask the artificial intelligence")
+async def search(interaction: discord.Interaction, question: str):
     try:
         # Generate a response using the GPT-3.5-turbo-0125 model
         response = openai.ChatCompletion.create(
@@ -40,14 +33,23 @@ async def ask(ctx, *args):
         answer = response['choices'][0]['message']['content'].strip()
 
         # Send the answer to Discord
-        await ctx.send(answer)
-
-    except openai.error.InvalidRequestError as e:
-        # Access the error message using the __str__ method
-        await ctx.send(f'Error: {e}')
+        print("There is a message")
+        await interaction.response.send_message(answer)
 
     except openai.error.OpenAIError as e:
-        await ctx.send(f'Error: {e}')
+        print(f'Error: {e.message}')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    await bot.change_presence(activity=discord.Game(name="/help"))
+    try:
+        synced = await bot.tree.sync()
+        print(f"Currently, there are {len(synced)} executable commands.")
+        print(pyfiglet.figlet_format(text="Confirmed"))
+    except Exception as e:
+        print("There is an issue with the bot. Please check the bot. Urgent.", e)
+        print(pyfiglet.figlet_format(text="Not Confirmed"))
 
 # Run the Discord bot
 bot.run(TOKEN)
